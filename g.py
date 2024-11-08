14dd7941823c4a9f13cd167285f2908c99588e25
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2024-11-08 07:50:27 -0800

    g

---
+++ g.py
@@ -0,0 +1,14 @@
+import torch
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, embed_dim, max_len=5000):
+        super().__init__()
+        position = torch.arange(0, max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, embed_dim, 2) * -(np.log(10000.0) / embed_dim))
+        pe = torch.zeros(max_len, embed_dim)
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        self.pe = pe.unsqueeze(0)
+
+    def forward(self, x):
+        return x + self.pe[:, :x.size(1), :].to(x.device)
--- scraping/main.py
+++ scraping/main.py
@@ -1,6 +1,10 @@
 import requests
 import time
 from tqdm import tqdm
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 url = "https://api.github.com/search/repositories"
 query = "language:Python size:5..5000 stars:>=100 template:false"
@@ -8,34 +12,36 @@ params = {
     "q": query,
     "per_page": 100
 }
-headers = {"Authorization":"no peeking"}
+headers = {"Authorization":"token "+os.environ["GITHUB_PAT"]}
 
-response = requests.get("https://api.github.com/rate_limit", headers=headers)
-rate_limit_data = response.json()
-print(rate_limit_data)
 def fetch_all_repositories():
     page = 1
     with open("repositories.txt", "w") as file:
         with tqdm(desc="Processing") as pbar:
-            
             while True:
-                pbar.update(1)
                 params["page"] = page
                 response = requests.get(url, headers=headers, params=params)
+                
                 if response.status_code == 200:
-                    print(response.headers["x-ratelimit-reset"])
                     data = response.json()
                     repositories = data.get("items", [])
                     for repo in repositories:
                         file.write(f"{repo['html_url']}\n")
+                    
                     if len(repositories) < params["per_page"]:
                         break
                     page += 1
-                    time.sleep(2)
+                    pbar.update(1)
+                    time.sleep(5)
+
+                elif response.status_code == 429:
+                    reset_time = int(response.headers.get("x-ratelimit-reset", 0))
+                    wait_time = max(reset_time - int(time.time()), 0)
+                    print(f"Rate limit exceeded. Waiting for {wait_time} seconds...")
+                    time.sleep(wait_time)
 
                 else:
                     print("Error:", response.status_code, response.json())
                     break
-
 
 fetch_all_repositories()
