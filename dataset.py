2cd981d70e610a1d3f5a66b3e76afba9325726b4
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2024-11-23 09:05:22 -0800

    ready to train

--- dataset.py
+++ dataset.py
@@ -208,7 +208,7 @@ dataset = TextCorpusDataset(
     vocab_size=10000,
     IS_CODE=True,
 )
-dset_size = int(len(dataset) / 20) # TODO: small for testing purposes
+dset_size = len(dataset)
 train_size = int(0.8 * dset_size)
 test_size = int(dset_size - train_size)
 
