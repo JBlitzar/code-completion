68353dcf5a6b9f39fe82d66196737d09e54ecf21
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2024-11-26 20:26:08 -0800

    BLACK!

--- archive-misc/wikitext.py
+++ archive-misc/wikitext.py
@@ -1,19 +1,22 @@
-from datasets import load_dataset # How presumptuous to have HF call their dataset library "datasets"
+from datasets import (
+    load_dataset,
+)  # How presumptuous to have HF call their dataset library "datasets"
 import os
 
 
 dataset = load_dataset("Salesforce/wikitext", "wikitext-103-raw-v1")
 
+
 def save_preprocessed(output_file, split="train"):
     data_split = dataset[split]
 
-
     separator = " <EOF> "
     all_text = separator.join(data_split["text"])
-
 
     with open(output_file, "w", encoding="utf-8") as f:
         f.write(all_text)
 
 
-save_preprocessed(os.path.expanduser("~/torch_datasets/wikitext/train/data/corpus_processed.txt"))
+save_preprocessed(
+    os.path.expanduser("~/torch_datasets/wikitext/train/data/corpus_processed.txt")
+)
