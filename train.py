9288d6a95a3637aa2cef159ad2250618393e9fe3
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2025-01-13 20:52:10 -0800

    wow!

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ from trainingmanager import TrainingManager
 import torch.nn as nn
 
 
-EXPERIMENT_DIRECTORY = "runs/code-decoder-v18-allTrains-customTokenizer"
+EXPERIMENT_DIRECTORY = "runs/code-decoder-v19-bigset-5k"
 
 
 device = "mps" if torch.backends.mps.is_available() else "cpu"
