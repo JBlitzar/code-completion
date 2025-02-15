22afc1a68bfcda8ad7e8a0b3716bb8ce668d98db
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2025-02-14 21:46:42 -0800

    anther quickfix brickstack

--- logger.py
+++ logger.py
@@ -24,7 +24,7 @@ def log_img(img, name):
 def init_logger(net, data=None, dir="runs"):
     net.eval()
     global writer
-    if not writer:
+    if not writer and writer.log_dir == dir:
         writer = SummaryWriter(dir)
     if data is not None:
         writer.add_graph(net, data)
