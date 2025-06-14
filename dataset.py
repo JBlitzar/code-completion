27ed4f46141642993d28e807617b4ad893d58de4
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2025-06-13 20:43:22 -0700

    pin memory

--- dataset.py
+++ dataset.py
@@ -923,7 +923,7 @@ def get_test_dataset():
 
 def get_dataloader(dataset, batch_size=64):
 
-    return DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    return DataLoader(dataset, batch_size=batch_size, shuffle=True, pin_memory=True)
 
 
 def fromDataset(dataset):
